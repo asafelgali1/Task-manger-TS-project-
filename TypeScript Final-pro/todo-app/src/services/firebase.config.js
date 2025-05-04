3c8ccd46d9b18fa177f457175ae28edc8707af8a
asafelgali1 <asafelgali23@gmail.com>
2025-05-04 13:14:04 +0300

    Update firebase.config.js

--- TypeScript Final-pro/todo-app/src/services/firebase.config.js
+++ TypeScript Final-pro/todo-app/src/services/firebase.config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyA0CZJVXc8TGlEm3Zo97kqwyoMIr_hFgn0",
-    authDomain: "task-manager-ff509.firebaseapp.com",
-    projectId: "task-manager-ff509",
-    storageBucket: "task-manager-ff509.firebasestorage.app",
-    messagingSenderId: "614710914650",
-    appId: "1:614710914650:web:1e288589e98bd1899cf263",
-    measurementId: "G-9FT3LLFLCE"
+    apiKey:-,
+    authDomain: -",
+    projectId: "-",
+    storageBucket: "-",
+    messagingSenderId: "-",
+    appId: "-",
+    measurementId: "-"
   };
 
 const app = initializeApp(firebaseConfig)
